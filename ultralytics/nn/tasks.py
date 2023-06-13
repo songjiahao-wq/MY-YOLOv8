8f634fdc8238a1b1141b8f0f9be4d102b9fd78c4
songjiahao-wq <1109790848@qq.com>
2023-06-13 21:30:52 +0800

    5.30

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -63,7 +63,7 @@ class BaseModel(nn.Module):
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
             if profile:
                 self._profile_one_layer(m, x, dt)
-            print(m)
+            # print(m)
             x = m(x)  # run
             y.append(x if m.i in self.save else None)  # save output
             if visualize:
